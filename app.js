e7d42fdb679578195760b385eba8abb1053b9c83
Manticora <999manticora999@gmail.com>
2023-04-06 15:04:16 +0300

    feat: fill in the variable a

--- app.js
+++ app.js
@@ -17,3 +17,25 @@ function clearAll() {
     finish = false; //
     out.textContent = 0;
 }
+
+document.querySelector('.ac').onclick = clearAll;
+
+document.querySelector('.buttons').onclick = (event) => {
+    // нажата не кнопка
+    if (!event.target.classList.contains('btn')) return;
+    // нажата кнопка ac
+    if (event.target.classList.contains('ac')) return;
+
+    out.textContent = '';
+    // получаю нажатую кнопку
+    const key = event.target.textContent;
+
+    // если нажата кнопка 0-9 или .
+    if (digit.includes(key)) {
+        a += key;
+        console.log(a, b, sign);
+        out.textContent = a;
+    };
+
+
+};
