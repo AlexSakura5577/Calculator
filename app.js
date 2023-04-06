8325a5198c7c1150f58808fa06f3c623aecadfd2
Manticora <999manticora999@gmail.com>
2023-04-06 14:44:29 +0300

    feat: add variables

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 let a = ''; // первое число
 let b = ''; // второе число
 let sign = ''; // знак операции
-let finish = ''; // первое число
+let finish = false; // 
+
+const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+const action = ['-', '+', 'X', '/'];
+
+// экран
+const out = document.querySelector('.calc-screen p');
+
+// очистить всё
+function clearAll() {
+    a = ''; // первое число
+    b = ''; // второе число
+    sign = ''; // знак операции
+    finish = false; //
+    out.textContent = 0;
+}
