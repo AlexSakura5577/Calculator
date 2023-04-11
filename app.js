623f1b37f998d4c207d275dff35a5d67090c3611
Manticora <999manticora999@gmail.com>
2023-04-11 15:11:08 +0300

    feat: complete numpad

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let a = ''; // первое число
 let b = ''; // второе число
 let sign = ''; // знак операции
 let finish = false; // 
-let keyNum;
+// let keyNum;
 
 const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
 const action = ['-', '+', '*', '/'];
@@ -18,19 +18,19 @@ function clearAll() {
     finish = false; //
     out.textContent = 0;
 }
-
+// нажата ac
 document.querySelector('.ac').onclick = clearAll;
 
 // ввод с клавиатуры:
-
 window.addEventListener("keypress", numPad, false);
 function numPad(event) {
     let keyNum = event.key;
     console.log("keyNum: " + keyNum);
+
     if (digit.includes(keyNum)) {
         if (b === '' && sign === '') {
             a += keyNum;
-            console.log("a:" + a, "b:" + b, "sign:" + sign);
+            console.log("a:" + a, "sign:" + sign, "b:" + b);
             out.textContent = a;
         }
         // если и первое и второе числа заполнены
@@ -43,14 +43,14 @@ function numPad(event) {
             b += keyNum;
             out.textContent = b;
         }
-        console.log("a:" + a, "b:" + b, "sign:" + sign);
+        console.log("a:" + a, "sign:" + sign, "b:" + b);
         return;
     };
     // если нажата кнопка + - * /
     if (action.includes(keyNum)) {
         sign = keyNum;
         out.textContent = sign;
-        console.log("a:" + a, "b:" + b, "sign:" + sign);
+        console.log("a:" + a, "sign:" + sign, "b:" + b);
         return;
     };
     // если нажата =
@@ -78,16 +78,13 @@ function numPad(event) {
                 break;
         }
         finish = true;
-        // вывести на экран
         out.textContent = a;
-        // console.log(a, b, sign);
-        console.log("a:" + a, "b:" + b, "sign:" + sign);
+        console.log("a:" + a, "sign:" + sign, "b:" + b);
         return;
     };
 };
 
 // ввод мышкой:
-
 document.querySelector('.buttons').onclick = (event) => {
     const key = event.target.textContent;
     console.log("keyMouse: " + key);
@@ -110,18 +107,16 @@ document.querySelector('.buttons').onclick = (event) => {
             b += key;
             out.textContent = b;
         }
-        console.log("a:" + a, "b:" + b, "sign:" + sign);
+        console.log("a:" + a, "sign:" + sign, "b:" + b);
         return;
     };
-
     // если нажата кнопка + - * /
     if (action.includes(key)) {
         sign = key;
         out.textContent = sign;
-        console.log("a:" + a, "b:" + b, "sign:" + sign);
+        console.log("a:" + a, "sign:" + sign, "b:" + b);
         return;
     };
-
     // если нажата =
     if (key === '=') {
         if (b === '') b = a;
@@ -147,13 +142,10 @@ document.querySelector('.buttons').onclick = (event) => {
                 break;
         }
         finish = true;
-        // вывести на экран
         out.textContent = a;
-        // console.log(a, b, sign);
-        console.log("a:" + a, "b:" + b, "sign:" + sign);
+        console.log("a:" + a, "sign:" + sign, "b:" + b);
         return;
     };
-
 };
 
 
