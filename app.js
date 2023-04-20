cd7c8670a62fe65aabd9460d96c0d48fb31c03a3
Manticora <999manticora999@gmail.com>
2023-04-20 16:48:38 +0300

    feat: created an object with properties

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
 // calc
-{}
+const calc = {
+    arrA: [], // первое число
+    arrB: [], // второе число
+    clear: function clearAll () {}, // очистить всё
+    delete: function deleteKey () {}, // очистка консоли и сброс клавишей delete
+    backspace: function backspaceKey () {}, // удалить по одной цифре справа налево клавишей backspace
+    comma: function commaKey () {}, // запятую в точку ru раскладка
+    mouse: function mouseE () {}, // ввод мышкой
+    keyboard: function keyboardE () {}, // ввод с клавиатуры
+    calculations: function calculationsEnd () {}, // вычисления
+};
+// цифры и точка
+const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+// знаки для вычислений
+const action = ['-', '+', '*', '/'];
+// экран вывода
+const out = document.querySelector('.calc-screen p');
