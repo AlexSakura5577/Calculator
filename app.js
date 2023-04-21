cbbbcb976c1ccadde06e88362fa56a203a815448
Manticora <999manticora999@gmail.com>
2023-04-21 15:57:07 +0300

    feat: found solutions for the event handler

--- app.js
+++ app.js
@@ -2,13 +2,13 @@
 const calc = {
     arrA: [], // первое число
     arrB: [], // второе число
-    clear: function clearAll () {}, // очистить всё
-    delete: function deleteKey () {}, // очистка консоли и сброс клавишей delete
-    backspace: function backspaceKey () {}, // удалить по одной цифре справа налево клавишей backspace
-    comma: function commaKey () {}, // запятую в точку ru раскладка
-    mouse: function mouseE () {}, // ввод мышкой
-    keyboard: function keyboardE () {}, // ввод с клавиатуры
-    calculations: function calculationsEnd () {}, // вычисления
+    clear: function clearAll() { }, // очистить всё
+    delete: function deleteKey() { }, // очистка консоли и сброс клавишей delete
+    backspace: function backspaceKey() { }, // удалить по одной цифре справа налево клавишей backspace
+    comma: function commaKey() { }, // запятую в точку ru раскладка
+    mouse: function mouseE() { }, // ввод мышкой
+    keyboard: function keyboardE() { }, // ввод с клавиатуры
+    calculations: function calculationsEnd() { }, // вычисления
 };
 // цифры и точка
 const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
@@ -16,3 +16,15 @@ const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
 const action = ['-', '+', '*', '/'];
 // экран вывода
 const out = document.querySelector('.calc-screen p');
+
+// клик мышкой
+// document.querySelector('.buttons').onclick = (event) => {
+//     let key = event.target.textContent;
+//     console.log("keyMouse: " + key);
+// };
+// ввод с клавиатуры
+window.addEventListener("keydown", numPad, false);
+function numPad(event) {
+    const key = event.key;
+    console.log("key: " + key);
+};
