d028e7f00f90d66cb11609a0683d2bd2b78f2314
Manticora <999manticora999@gmail.com>
2023-04-25 15:14:55 +0300

    feat: add two event handlers with one variable

--- app.js
+++ app.js
@@ -1,16 +1,49 @@
+
+
+// цифры и точка
+const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+// знаки для вычислений
+const action = ['-', '+', '*', '/'];
+// экран вывода
+const out = document.querySelector('.calc-screen p');
+// const paragraph = document.querySelector('.pole_ebanoe_blyat');
+
+// кнопка (селектор)
+const btn = document.querySelector('.buttons')
+
+// вывод на экран (переделать)
+// const changeParagraph = (key) => {
+//   if (!isNaN(key)) {
+//     out.innerText += key;
+//   }
+// }
+
+
+
 // calc
 const calc = {
 
+
+
   key: window.addEventListener('keydown', (e) => {
-    const key = e.key;
-    changeParagraph(e.key)
-    this.arrA.push(key);
-    console.log(arrA);
+    let key = e.key;
+    // changeParagraph(e.key)
+    out.textContent = key;
+    calc.arrA.push(key);
+    console.log(calc.arrA);
   }),
 
+  key: btn.addEventListener('click', (e) => {
+    let key = e.target.textContent;
+    // changeParagraph(e.target.innerText)
+    out.textContent = key;
+    calc.arrA.push(key);
+    console.log(calc.arrA);
+  }),
 
   arrA: [], // первое число
   arrB: [], // второе число
+
   clear: function clearAll() { }, // очистить всё
   delete: function deleteKey() { }, // очистка консоли и сброс клавишей delete
   backspace: function backspaceKey() { }, // удалить по одной цифре справа налево клавишей backspace
@@ -23,26 +56,7 @@ const calc = {
     console.log('клик по кнопке');
   } // сообщение
 };
-// цифры и точка
-const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
-// знаки для вычислений
-const action = ['-', '+', '*', '/'];
-// экран вывода
-const out = document.querySelector('.calc-screen p');
-// const paragraph = document.querySelector('.pole_ebanoe_blyat');
-const buttons = document.querySelector('.buttons');
 
-// window.addEventListener('keydown', (e) => {
-//   changeParagraph(e.key)
-// })
 
-buttons.addEventListener('click', (e) => {
-  changeParagraph(e.target.innerText)
-})
 
-const changeParagraph = (key) => {
-  if (!isNaN(key)) {
-    out.innerText += key;
-  }
-}
 
