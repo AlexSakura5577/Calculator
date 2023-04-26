ebb0168e1ace7248e3da8ad831ad61c0d31ab70e
Manticora <999manticora999@gmail.com>
2023-04-26 14:23:53 +0300

    feat: add line output

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+// Calculator
 
 // цифры и точка
 const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
@@ -13,28 +13,35 @@ const btn = document.querySelector('.buttons')
 // calc
 const calc = {
 
-
-
   key: window.addEventListener('keydown', (e) => {
     let key = e.key;
-    // changeParagraph(e.key)
     calc.arrA.push(key);
     calc.output();
-    // out.textContent = key;
     console.log(calc.arrA);
+    // return calc.arrA;
   }),
 
-  key: btn.addEventListener('click', (e) => {
-    let key = e.target.textContent;
-    // changeParagraph(e.target.innerText)
-    // out.textContent = key;
-    calc.arrA.push(key);
-    console.log(calc.arrA);
-  }),
+  // key: btn.addEventListener('click', (e) => {
+  //   let key = e.target.textContent;
+  //   calc.arrA.push(key);
+  //   console.log(calc.arrA);
+  // }),
 
   arrA: [], // первое число
   arrB: [], // второе число
 
+  output: function output() {
+    out.textContent = calc.arrA.join('');
+  }, // окно вывода
+
+
+  // message: function consoleMessage() {
+  //   console.log('клик по кнопке');
+  // } // сообщение
+};
+
+
+/*
   clear: function clearAll() { }, // очистить всё
   delete: function deleteKey() { }, // очистка консоли и сброс клавишей delete
   backspace: function backspaceKey() { }, // удалить по одной цифре справа налево клавишей backspace
@@ -42,14 +49,5 @@ const calc = {
   mouse: function mouseE() { }, // ввод мышкой
   keyboard: function keyboardE() { }, // ввод с клавиатуры
   calculations: function calculationsEnd() { }, // вычисления
-  output: function outputWin() {
-    out.textContent = key;
-  }, // окно вывода
-  message: function consoleMessage() {
-    console.log('клик по кнопке');
-  } // сообщение
-};
-
-
-
+*/
 
