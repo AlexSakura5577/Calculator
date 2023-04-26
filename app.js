d7e38f8548efbaa821e684f639e96da89e66553c
Manticora <999manticora999@gmail.com>
2023-04-26 16:03:26 +0300

    feat: nice

--- app.js
+++ app.js
@@ -8,27 +8,42 @@ const action = ['-', '+', '*', '/'];
 const out = document.querySelector('.calc-screen p');
 // кнопка (селектор)
 const btn = document.querySelector('.buttons')
+
 // calc
 const calc = {
 
   arrA: [], // первое число
   arrB: [], // второе число
 
-  key: window.addEventListener('keydown', (e) => {
-    let key = e.key;
-    calc.arrA.push(key);
-    calc.output(calc.arrA);
-    console.log(calc.arrA);
+  key: function key() {
+    let key;
+    event1 = window.addEventListener('keydown', (e) => {
+      key = e.key;
+      console.log(key);
+      return key;
+    })
+    event2 = btn.addEventListener('click', (e) => {
+      key = e.target.textContent;
+      console.log(key);
+      return key;
+    })
     return;
-  }),
+  },
 
-  key: btn.addEventListener('click', (e) => {
-    let key = e.target.textContent;
-    calc.arrA.push(key);
-    calc.output(calc.arrA);
-    console.log(calc.arrA);
-    return;
-  }),
+
+  // key: window.addEventListener('keydown', (e) => {
+  //   let key = e.key;
+  //   return;
+  // }),
+
+  // key: btn.addEventListener('click', (e) => {
+  //   let key = e.target.textContent;
+  //   return;
+  // }),
+
+  // calc.arrA.push(key);
+  // calc.output(calc.arrA);
+  // console.log(calc.arrA);
 
   // str1: 
 
@@ -40,10 +55,19 @@ const calc = {
   }, // окно вывода
 
 
-  // message: function consoleMessage() {
-  //   console.log('клик по кнопке');
-  // } // сообщение
+  message: function consoleMessage() {
+    calc.key();
+    // events = calc.key();
+    // console.log(events);
+    // console.log(calc.key());
+    // calc.arrA.push(calc.key());
+    // calc.output(calc.arrA);
+    // console.log(calc.arrA);
+  } // сообщение
 };
+
+// calc.key();
+calc.message();
 
 
 
