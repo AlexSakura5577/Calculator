cc351fe598d7f48d7d256cd14e058e01e20365d9
Manticora <999manticora999@gmail.com>
2023-04-27 15:09:59 +0300

    feat: callback function works!

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Calculator
-
+inputLine = '';
 // цифры и точка
 const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
 // знаки для вычислений
@@ -15,23 +15,20 @@ const calc = {
   arrA: [], // первое число
   arrB: [], // второе число
 
-  key: function key(cb) {
+  key: function myFunc(callback) {
     let key;
     event1 = window.addEventListener('keydown', (e) => {
       key = e.key;
-      cb(e.key);
+      callback(key);
       // console.log(key);
-      return key;
     })
     event2 = btn.addEventListener('click', (e) => {
       key = e.target.textContent;
-      cb(e.target.textContent);
+      callback(key);
       // console.log(key);
-      return key;
     })
     return;
   },
-
 
   // key: window.addEventListener('keydown', (e) => {
   //   let key = e.key;
@@ -65,16 +62,32 @@ const calc = {
     // calc.arrA.push(calc.key());
     // calc.output(calc.arrA);
     // console.log(calc.arrA);
-  } // сообщение
+  }, // сообщение
+
+
 };
 
 // inputLine = '';
+// console.log('inputLine: ' + inputLine);
 // calc.key();
 // calc.message();
-calc.key(key => console.log(key))
+// calc.key(key => console.log(key)) // key в консоль
 // calc.key(key => inputLine = key);
 // console.log(inputLine);
+// calc.key(key => console.log(key))
 
+// calc.key(key => {
+//   // вот тут пиши дальше код который использует key
+//   inputLine = key;
+//   console.log('inputLine: ' + inputLine);
+// })
+
+function out2(key) {
+  inputLine = key;
+  console.log('inputLine: ' + inputLine);
+}
+// myFunc(out2);
+calc.key(out2);
 
 
 
